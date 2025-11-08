fa2979d0dfaea83cbf71e153120b2c000c7d902e
khoanguyen22 <khoa.nguyen2@saigontechnology.com>
2025-11-08 19:58:08 +0700

    feat: add ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ export const metadata: Metadata = {
   title: "GLVIETSUB - Xem Phim Online HD",
   description:
     "Trang Vietsub phim GL hay nhất hoàn toàn miễn phí, cập nhật nhanh chóng và đầy đủ.",
-  keywords: "phim online, xem phim, phim HD, phụ đề việt, phim GL, vietsub, GLVIETSUB, vietsub phim, phim vietsub, phim vietsub online, phim vietsub HD, phim vietsub hay nhất, phim vietsub miễn phí, phim vietsub cập nhật nhanh chóng và đầy đủ.",
+  keywords:
+    "phim online, xem phim, phim HD, phụ đề việt, phim GL, vietsub, GLVIETSUB, vietsub phim, phim vietsub, phim vietsub online, phim vietsub HD, phim vietsub hay nhất, phim vietsub miễn phí, phim vietsub cập nhật nhanh chóng và đầy đủ.",
   icons: {
     icon: [
       { url: "/assets/logo.ico", sizes: "any" },
@@ -14,6 +15,9 @@ export const metadata: Metadata = {
     ],
     shortcut: "/assets/logo.ico",
     apple: "/assets/logo.ico",
+  },
+  other: {
+    "6a97888e-site-verification": "7dede8a0dd96413eb8c8887cd0ffd25b",
   },
 };
 
