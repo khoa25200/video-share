cd15ca1c1188473661fba4716e3745ea436374d3
dangkhoa2520ss-dot <dangkhoa2520ss@gmail.com>
2025-11-14 22:59:41 +0700

    add ads

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-  domain: "5gvci.com",
-  zoneId: 10187969,
+  domain: "3nbf4.com",
+  zoneId: 10188024,
 };
 self.lary = "";
-importScripts("https://5gvci.com/act/files/service-worker.min.js?r=sw");
+importScripts("https://3nbf4.com/act/files/service-worker.min.js?r=sw");
